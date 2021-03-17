3641a257d7f80be96fd2046483c4173d8e2005fd
Arvid A <arvid.anderson@outlook.com>
2021-03-17 13:05:10 +0100

    Update numpy_05.py

--- numpy_05.py
+++ numpy_05.py
@@ -1,4 +1,2 @@
 from matplotlib import pyplot as plt; import numpy as np; 
 #Work in progress
-x = np.arange(-2,3); y = x**2-1
-plt.title('SELK-BREWERY'); plt.xlabel('x'); plt.ylabel('y'); plt.plot(x,y); plt.show()
