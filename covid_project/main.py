0727b7ac23fff40e7cceda96418dccc3d9310bbf
Arvid A <arvid.anderson@outlook.com>
2021-04-19 23:01:38 +0200

    Changed folder structure, made get directory  and discovering pandas

--- covid_project/main.py
+++ covid_project/main.py
@@ -1,5 +1,5 @@
 # Arvid Anderson TE19D, https://github.com/arvidanderson
 
 #Imports
-from data_files import * # Importing my file library to avoid bloat in this file
+from get.get_DataFiles import * # Importing my file library to avoid bloat in this file
 
--- covid_project/warmup.py
+++ covid_project/warmup.py
@@ -2,14 +2,14 @@
 
 import pandas as pd
 import matplotlib.pyplot as plt
-from data_files import *
+from get.get_DataFiles import *
 
 df = pd.read_csv(NationalDailyDeaths, delimiter=',')
 Dates = list(df['Date'])
 DailyDeaths = list(df['National_Daily_Deaths'])
 
 plt.plot(Dates, DailyDeaths, color='red', marker='o')
-plt.title('Antal döda per dag i Covid-19 (Sverige)', fontsize=14)
+plt.title('People dead per day in Covid-19 (Sweden)', fontsize=14)
 plt.xlabel('Day', fontsize=15)
 plt.ylabel('Deaths', fontsize=15)
 plt.grid(True)
